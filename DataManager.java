ad8deb11ae4bedf5e61aaebf82240d21cb06c1ec
wmbraun <55810196+wmbraun@users.noreply.github.com>
2020-04-29 15:34:40 -0500

    DataManager.java
    Still no calculation by field, all other methods should work.

--- DataManager.java
+++ DataManager.java
@@ -3,6 +3,8 @@ package application;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
 import java.util.Set;
 
 public class DataManager {
@@ -22,7 +24,7 @@ public class DataManager {
 	 * Calculates the average weight of milk produced by farms in a given month.
 	 * 
 	 * @param month
-	 * @return
+	 * @return average
 	 */
 	public Integer getMonthlyAverage(String month) {
 
@@ -32,7 +34,7 @@ public class DataManager {
 
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
-			ArrayList<String> yearsList = new ArrayList<String>();// list of years to act as count
+			ArrayList<String> yearsList = new ArrayList<String>();// list of years to act as count for each farm
 			// stores the date values of each data entry, easier to parse
 			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 			// dates of entry are matched to specified date
@@ -57,37 +59,60 @@ public class DataManager {
 	}
 
 	/**
-	 * Calculates the minimum weight of milk produced by farms in a given month and
-	 * year.
+	 * Calculates the minimum weight of milk produced by farms in a given month.
 	 * 
 	 * @param month
-	 * @return
+	 * @return min
 	 */
 	public Integer getMonthlyMin(String month) {
 		int min = 0;// monthly minimum value
 		int initialMin = 0;// used to set to a correct starting value
-
+		Set<String> keys = new HashSet<String>();// holds keys for each set used in search
+		Hashtable<String, Integer> monthKeys = new Hashtable<String, Integer>();// keys that have the correct month
+		Hashtable<String, Integer> yearSum = new Hashtable<String, Integer>();// sums the total yield for the month in a
+																				// given year
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
 			// stores the date values of each data entry, easier to parse
-			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+			keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 			// dates of entry are matched to specified date
 			for (String key : keys) {
 				String[] values = key.toString().split("-");
 				for (int j = 0; j < values.length; j += 3) {
-					// if a date matches in month and year then it added to total
+					// if a date matches in month
 					if (month.equals(values[j + 1])) {
-						// set the min value to first recorded value as it is currently 0
-						if (0 == initialMin) {
-							min = factory.milkDataFromFarms.get(i).milkData.get(key);
-							initialMin++;
-						}
-						// compare current value to min value
-						if (min > factory.milkDataFromFarms.get(i).milkData.get(key)) {
-							min = factory.milkDataFromFarms.get(i).milkData.get(key);
-						}
+						monthKeys.put(key, factory.milkDataFromFarms.get(i).milkData.get(key));
 					}
 				}
+			}
+		}
+		keys = monthKeys.keySet();
+		for (String key : keys) {
+			String[] values = key.toString().split("-");
+			for (int j = 0; j < values.length; j += 3) {
+				// if year is not in list
+				if (!yearSum.containsKey(values[j])) {
+					yearSum.put(values[j], monthKeys.get(key));
+				}
+				// if year is in table replace with updated total
+				else {
+					int sum = monthKeys.get(key) + yearSum.get(values[j]);
+					yearSum.put(values[j], sum);
+				}
+			}
+		}
+
+		keys = yearSum.keySet();
+		// dates of entry are matched to specified date
+		for (String key : keys) {
+			// set the min value to first recorded value as it is currently 0
+			if (0 == initialMin) {
+				min = yearSum.get(key);
+				initialMin++;
+			}
+			// compare current value to min value
+			if (min > yearSum.get(key)) {
+				min = yearSum.get(key);
 			}
 		}
 		return min;
@@ -101,27 +126,54 @@ public class DataManager {
 	 * @return
 	 */
 	public Integer getMonthlyMax(String month) {
+
 		int max = 0;// maximum weight
+		Set<String> keys = new HashSet<String>();// holds keys for each set used in search
+		Hashtable<String, Integer> monthKeys = new Hashtable<String, Integer>();// keys that have the correct month
+		Hashtable<String, Integer> yearSum = new Hashtable<String, Integer>();// sums the total yield for the month in a
+																				// given year
 
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
 			// stores the date values of each data entry, easier to parse
-			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+			keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 			// dates of entry are matched to specified date
 			for (String key : keys) {
 				String[] values = key.toString().split("-");
 				for (int j = 0; j < values.length; j += 3) {
-					// if a date matches in month and year then it added to total
+					// if a date matches in month
 					if (month.equals(values[j + 1])) {
-						// compare max value for current value
-						if (max < factory.milkDataFromFarms.get(i).milkData.get(key)) {
-							max = factory.milkDataFromFarms.get(i).milkData.get(key);
-						}
+						monthKeys.put(key, factory.milkDataFromFarms.get(i).milkData.get(key));
 					}
 				}
 			}
 		}
+		keys = monthKeys.keySet();
+		for (String key : keys) {
+			System.out.println(key + " monthkey");
+			String[] values = key.toString().split("-");
+			for (int j = 0; j < values.length; j += 3) {
+				// if year is not in list
+				if (!yearSum.containsKey(values[j])) {
+					yearSum.put(values[j], monthKeys.get(key));
+				}
+				// if year is in table replace with updated total
+				else {
+					int sum = monthKeys.get(key) + yearSum.get(values[j]);
+					yearSum.put(values[j], sum);
+				}
+			}
+		}
+		keys = yearSum.keySet();
+		System.out.println(keys + "years");
+		// dates of entry are matched to specified date
+		for (String key : keys) {
+			if (max < yearSum.get(key)) {
+				max = yearSum.get(key);
+			}
+		}
 		return max;
+
 	}
 
 	/**
@@ -175,33 +227,56 @@ public class DataManager {
 	 * @return
 	 */
 	public Integer getMonthlyMinForFarm(String farm, String month) {
-
 		int min = 0;// monthly minimum value
 		int initialMin = 0;// used to set to a correct starting value
-
+		Set<String> keys = new HashSet<String>();// holds keys for each set used in search
+		Hashtable<String, Integer> monthKeys = new Hashtable<String, Integer>();// keys that have the correct month
+		Hashtable<String, Integer> yearSum = new Hashtable<String, Integer>();// sums the total yield for the month in a
+																				// given year
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
 			if (farm.equals(factory.milkDataFromFarms.get(i).farmID)) {
 				// stores the date values of each data entry, easier to parse
-				Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+				keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 				// dates of entry are matched to specified date
 				for (String key : keys) {
 					String[] values = key.toString().split("-");
 					for (int j = 0; j < values.length; j += 3) {
-						// if a date matches in month and year then it added to total
+						// if a date matches in month
 						if (month.equals(values[j + 1])) {
-							// set the min value to first recorded value as it is currently 0
-							if (0 == initialMin) {
-								min = factory.milkDataFromFarms.get(i).milkData.get(key);
-								initialMin++;
-							}
-							// compare current value to min value
-							if (min > factory.milkDataFromFarms.get(i).milkData.get(key)) {
-								min = factory.milkDataFromFarms.get(i).milkData.get(key);
-							}
+							monthKeys.put(key, factory.milkDataFromFarms.get(i).milkData.get(key));
 						}
 					}
 				}
+			}
+		}
+		keys = monthKeys.keySet();
+		for (String key : keys) {
+			String[] values = key.toString().split("-");
+			for (int j = 0; j < values.length; j += 3) {
+				// if year is not in list
+				if (!yearSum.containsKey(values[j])) {
+					yearSum.put(values[j], monthKeys.get(key));
+				}
+				// if year is in table replace with updated total
+				else {
+					int sum = monthKeys.get(key) + yearSum.get(values[j]);
+					yearSum.put(values[j], sum);
+				}
+			}
+		}
+
+		keys = yearSum.keySet();
+		// dates of entry are matched to specified date
+		for (String key : keys) {
+			// set the min value to first recorded value as it is currently 0
+			if (0 == initialMin) {
+				min = yearSum.get(key);
+				initialMin++;
+			}
+			// compare current value to min value
+			if (min > yearSum.get(key)) {
+				min = yearSum.get(key);
 			}
 		}
 		return min;
@@ -216,29 +291,56 @@ public class DataManager {
 	 * @return
 	 */
 	public Integer getMonthlyMaxForFarm(String farm, String month) {
+
 		int max = 0;// maximum weight
+		Set<String> keys = new HashSet<String>();// holds keys for each set used in search
+		Hashtable<String, Integer> monthKeys = new Hashtable<String, Integer>();// keys that have the correct month
+		Hashtable<String, Integer> yearSum = new Hashtable<String, Integer>();// sums the total yield for the month in a
+																				// given year
 
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
 			if (farm.equals(factory.milkDataFromFarms.get(i).farmID)) {
 				// stores the date values of each data entry, easier to parse
-				Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+				keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 				// dates of entry are matched to specified date
 				for (String key : keys) {
 					String[] values = key.toString().split("-");
 					for (int j = 0; j < values.length; j += 3) {
-						// if a date matches in month and year then it added to total
+						// if a date matches in month
 						if (month.equals(values[j + 1])) {
-							// compare max value for current value
-							if (max < factory.milkDataFromFarms.get(i).milkData.get(key)) {
-								max = factory.milkDataFromFarms.get(i).milkData.get(key);
-							}
+							monthKeys.put(key, factory.milkDataFromFarms.get(i).milkData.get(key));
 						}
 					}
 				}
 			}
 		}
+		keys = monthKeys.keySet();
+		for (String key : keys) {
+			System.out.println(key + " monthkey");
+			String[] values = key.toString().split("-");
+			for (int j = 0; j < values.length; j += 3) {
+				// if year is not in list
+				if (!yearSum.containsKey(values[j])) {
+					yearSum.put(values[j], monthKeys.get(key));
+				}
+				// if year is in table replace with updated total
+				else {
+					int sum = monthKeys.get(key) + yearSum.get(values[j]);
+					yearSum.put(values[j], sum);
+				}
+			}
+		}
+		keys = yearSum.keySet();
+		System.out.println(keys + "years");
+		// dates of entry are matched to specified date
+		for (String key : keys) {
+			if (max < yearSum.get(key)) {
+				max = yearSum.get(key);
+			}
+		}
 		return max;
+
 	}
 
 	public Integer getDataSortedByField() {
@@ -249,20 +351,22 @@ public class DataManager {
 	/**
 	 * Average weight within a specified date range, average weight by day
 	 * 
-	 * @param day
-	 * @param month
-	 * @param year
+	 * @param day1 -start
+	 * @param month1
+	 * @param year1
+	 * @param day2 -end
+	 * @param month2
+	 * @param year2
 	 * @return
 	 */
-	public Integer getAverageInDateRange(String day1, String month1, String year1,String day2, String month2, String year2) {
+	public Integer getAverageInDateRange(String day1, String month1, String year1, String day2, String month2,
+			String year2) {
 		int total = 0;// total amount of milk weight between all farms
 		int average = 0;// average amount of weight in a given month
-//		int yearCount = 0;// number of years observed
 
 		// loops through farms in a given factory
 		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
-//			ArrayList<String> yearsList = new ArrayList<String>();// list of years to act as count
-//			ArrayList<String> monthsList = new ArrayList<String>();// list of months to act as count
+
 			// stores the date values of each data entry, easier to parse
 			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
 			// dates of entry are matched to specified date
@@ -283,33 +387,108 @@ public class DataManager {
 			}
 
 		}
+		//calculation of the difference between two dates
 		LocalDate dateBefore = LocalDate.of(Integer.parseInt(year1), Integer.parseInt(month1), Integer.parseInt(day1));
 		LocalDate dateAfter = LocalDate.of(Integer.parseInt(year2), Integer.parseInt(month2), Integer.parseInt(day2));
 		long diffOfDays = ChronoUnit.DAYS.between(dateBefore, dateAfter);
+		if (diffOfDays != 0) {
 		average = (int) (total / diffOfDays);
+		}else {
+			average = total;
+		}
 		return average;
 
 	}
 
 	/**
+	 * Calculates the maximum weight of milk produced on a given day in range of 2 dates
 	 * 
-	 * @param day
-	 * @param month
-	 * @param year
+	 * @param day1
+	 * @param month1
+	 * @param year1
+	 * @param day2
+	 * @param month2
+	 * @param year2
 	 * @return
 	 */
-	public Integer getMinInDateRange(String day1, String month1, String year1,String day2, String month2, String year2) {
-		return null;
+	public Integer getMinInDateRange(String day1, String month1, String year1, String day2, String month2,
+			String year2) {
+		int min = 0;// monthly minimum value
+		int initialMin = 0;// used to set to a correct starting value
+
+		// loops through farms in a given factory
+		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
+
+			// stores the date values of each data entry, easier to parse
+			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+			// dates of entry are matched to specified date
+			for (String key : keys) {
+				String[] values = key.toString().split("-");
+				for (int j = 0; j < values.length; j += 3) {
+					// if a date matches in month and year then it added to total
+					if ((Integer.parseInt(year1) <= Integer.parseInt(values[j])
+							&& Integer.parseInt(month1) <= Integer.parseInt(values[j + 1])
+							&& Integer.parseInt(day1) <= Integer.parseInt(values[j + 2]))
+							&& (Integer.parseInt(year2) >= Integer.parseInt(values[j])
+									&& Integer.parseInt(month2) >= Integer.parseInt(values[j + 1])
+									&& Integer.parseInt(day2) >= Integer.parseInt(values[j + 2]))) {
+						if (0 == initialMin) {
+							min = factory.milkDataFromFarms.get(i).milkData.get(key);
+							initialMin++;
+						}
+						// compare current value to min value
+						if (min > factory.milkDataFromFarms.get(i).milkData.get(key)) {
+							min = factory.milkDataFromFarms.get(i).milkData.get(key);
+						}
+					}
+				}
+
+			}
+
+		}
+		return min;
 	}
 
 	/**
+	 * Calculates the minimum weight of milk produced on a given day in range of 2 dates
 	 * 
-	 * @param day
-	 * @param month
-	 * @param year
+	 * @param day1
+	 * @param month1
+	 * @param year1
+	 * @param day2
+	 * @param month2
+	 * @param year2
 	 * @return
 	 */
-	public Integer getMaxInDateRange(String day1, String month1, String year1,String day2, String month2, String year2) {
-		return null;
+	public Integer getMaxInDateRange(String day1, String month1, String year1, String day2, String month2,
+			String year2) {
+		int max = 0;// maximum weight
+
+		// loops through farms in a given factory
+		for (int i = 0; i < factory.milkDataFromFarms.size(); i++) {
+
+			// stores the date values of each data entry, easier to parse
+			Set<String> keys = factory.milkDataFromFarms.get(i).milkData.keySet();
+			// dates of entry are matched to specified date
+			for (String key : keys) {
+				String[] values = key.toString().split("-");
+				for (int j = 0; j < values.length; j += 3) {
+					// if a date matches in month and year then it added to total
+					if ((Integer.parseInt(year1) <= Integer.parseInt(values[j])
+							&& Integer.parseInt(month1) <= Integer.parseInt(values[j + 1])
+							&& Integer.parseInt(day1) <= Integer.parseInt(values[j + 2]))
+							&& (Integer.parseInt(year2) >= Integer.parseInt(values[j])
+									&& Integer.parseInt(month2) >= Integer.parseInt(values[j + 1])
+									&& Integer.parseInt(day2) >= Integer.parseInt(values[j + 2]))) {
+						if (max < factory.milkDataFromFarms.get(i).milkData.get(key)) {
+							max = factory.milkDataFromFarms.get(i).milkData.get(key);
+						}
+					}
+				}
+
+			}
+
+		}
+		return max;
 	}
 }
